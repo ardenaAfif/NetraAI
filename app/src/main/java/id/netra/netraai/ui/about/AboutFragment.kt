47269ced7fb4737c396356424fe341f90c3e3c44
aafif <aafifpratama@gmail.com>
2024-11-21 16:04:53 +0700

    [Afif] Added content di About

--- app/src/main/java/id/netra/netraai/ui/about/AboutFragment.kt
+++ app/src/main/java/id/netra/netraai/ui/about/AboutFragment.kt
@@ -5,15 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isInvisible
 import id.netra.netraai.R
+import id.netra.netraai.databinding.FragmentAboutBinding
 
 class AboutFragment : Fragment() {
+
+    private var _binding: FragmentAboutBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false)
+    ): View {
+        _binding = FragmentAboutBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        customToolbar()
+    }
+
+    private fun customToolbar() {
+        binding.apply {
+            toolbar.navBack.isInvisible = true
+            toolbar.tvToolbarName.text = getString(R.string.app_name)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
--- app/src/main/java/id/netra/netraai/ui/home/HomeFragment.kt
+++ app/src/main/java/id/netra/netraai/ui/home/HomeFragment.kt
@@ -5,16 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isInvisible
 import id.netra.netraai.R
+import id.netra.netraai.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        customToolbar()
+    }
+
+    private fun customToolbar() {
+        binding.apply {
+            toolbar.navBack.isInvisible = true
+            toolbar.tvToolbarName.text = getString(R.string.app_name)
+        }
     }
 
 }
