cc1f98bc258a203e3d273e50d164d69b5d077b73
amarsh30 <rzhamr06@gmail.com>
2024-11-21 21:24:58 +0700

    add slicing ubah materi ke suara (button close dan seekbar belum resolve)

---
+++ app/src/main/java/id/netra/netraai/ui/convertvoice/ConvertVoiceActivity.kt
@@ -0,0 +1,21 @@
+package id.netra.netraai.ui.convertvoice
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import id.netra.netraai.R
+
+class ConvertVoiceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_convert_voice)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
