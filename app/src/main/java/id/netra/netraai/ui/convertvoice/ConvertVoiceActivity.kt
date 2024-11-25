ef12779ba8f19cd6823c051a8a4336c50e7b28ef
aafif <aafifpratama@gmail.com>
2024-11-26 05:42:09 +0700

    [Afif] Updated convert voice design

--- app/src/main/java/id/netra/netraai/ui/convertvoice/ConvertVoiceActivity.kt
+++ app/src/main/java/id/netra/netraai/ui/convertvoice/ConvertVoiceActivity.kt
@@ -6,16 +6,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import id.netra.netraai.R
+import id.netra.netraai.databinding.ActivityConvertVoiceBinding
 
 class ConvertVoiceActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityConvertVoiceBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_convert_voice)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityConvertVoiceBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/id/netra/netraai/ui/home/HomeFragment.kt
+++ app/src/main/java/id/netra/netraai/ui/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package id.netra.netraai.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,6 +9,7 @@ import android.view.ViewGroup
 import androidx.core.view.isInvisible
 import id.netra.netraai.R
 import id.netra.netraai.databinding.FragmentHomeBinding
+import id.netra.netraai.ui.convertvoice.ConvertVoiceActivity
 
 class HomeFragment : Fragment() {
 
@@ -26,6 +28,15 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         customToolbar()
+        actionListener()
+    }
+
+    private fun actionListener() {
+        binding.cardDocToVoice.setOnClickListener {
+            Intent(requireContext(), ConvertVoiceActivity::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 
     private fun customToolbar() {
